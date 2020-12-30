f55af71b67a8d0661175003894cdd1c95ba69a63
--replace-all <itsimmi5@gmail.com>
2020-12-30 13:09:46 -0500

    Feat: added loop with hints for user.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-# computer will pick the number
-#  show rules, 
-#  user write number, 
-#  check if its the same number, 
-#  else give hint
-
 from numpy import random
 
 num = random.randint(10)
 print("Guess number from 1 - 10")
 userInput = int(input())
 
-
 while userInput!= num:
-    userInput = int(input("Wrong guess, try again? "))
+    # if user guess is wrong
+    if userInput > num:
+        userInput = int(input("Wrong guess, your guess is more than actual answer. Try again "))
+    if userInput < num:
+        userInput = int(input("Wrong guess, your guess is less than actual answer. Try again "))
 
+   # if user guess the right number
     if userInput == num:
         print("Congratulations!! you guessed the right number")
+        print("Game Over, You WIN!!!")
